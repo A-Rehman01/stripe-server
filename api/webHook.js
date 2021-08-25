6762a5f00b3294cfaef78cd928993daa6dcfa0ec
A-Rehman01 <arehmana377@gmail.com>
2021-08-25 13:38:17 +0500

    add custom

--- api/webHook.js
+++ api/webHook.js
@@ -1,5 +1,19 @@
 const stripeAPI = require('../stripe');
 
+const webHookHandlers = {
+  'checkout.session.completed': (data) => {
+    console.log('Checkout completed successfully', data);
+    // other business logic
+  },
+
+  'payment_intent.succeeded': (data) => {
+    console.log('Payment succeeded', data);
+  },
+  'payment_intent.payment_failed': (data) => {
+    console.log('Payment Failed', data);
+  },
+};
+
 const webhook = (req, res) => {
   const sig = req.headers['stripe-signature'];
   let event;
@@ -14,9 +28,8 @@ const webhook = (req, res) => {
     return res.status(400).send(`WebHook Error ${error.message}`);
   }
 
-  if (event.type === 'checkout.session.completed') {
-    const session = event.data.object;
-    console.log('Event data', session);
+  if (webHookHandlers[event.type]) {
+    webHookHandlers[event.type](event.data.object);
   }
 };
 
